a393ee5218c1ae764d409c0e75c0e0b95408574c
Stas Kelvich <stas.kelvich@gmail.com>
2022-06-08 14:49:21 +0300

    set proper SITE_URL in fetch() call

--- pages/drafts.tsx
+++ pages/drafts.tsx
@@ -39,7 +39,7 @@ const Drafts: React.FC<Props> = (props) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await fetch("http://localhost:3000/api/drafts")
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/drafts`)
   const drafts = await res.json()
   return {
     props: { drafts },
