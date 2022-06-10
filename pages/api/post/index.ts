a6d6f3c6fcca65f41f3549321b0aeb5d5fb04587
Stas Kelvich <stas.kelvich@gmail.com>
2022-06-10 10:55:52 +0300

    Add users tab

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -6,12 +6,22 @@ import prisma from '../../../lib/prisma'
 // Required fields in body: title, authorEmail
 // Optional fields in body: content
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const { title, content, authorEmail } = req.body
+  const { title, content, authorEmail, authorName } = req.body
   const result = await prisma.post.create({
     data: {
       title: title,
       content: content,
-      author: { connect: { email: authorEmail } },
+      author: {
+        connectOrCreate: {
+          where: {
+            email: authorEmail,
+          },
+          create: {
+            email: authorEmail,
+            name: authorName,
+          },
+        },
+      },
     },
   })
   res.json(result)
